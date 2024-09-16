369469bcce726c6a9123381949306bb145bd329e
manuelaelosantos <163124866+manuelaelosantos@users.noreply.github.com>
2024-09-16 11:34:06 -0300

    .

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const nuvem = document.querySelector(".nuvem");
 const startButton = document.querySeeetor(".start");
 const gameOverScreen = document.querySelector(".game=over");
 
-audioStart = new Audio("./sound/audio_theme.mp3);
-const gameOverSound = new Audio("./sound/audio_gameover.mp3);
+audioStart = new Audio("./sound/audio_theme.mp3");
+const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
 
-const startGame => {
+const startGame = () => {
 gameStarded = true;
 audioStart.play();
 pipe.style.animation = "pipe-animation 1.5s infinite linear";
@@ -52,9 +52,9 @@ const jump = () => {
 
     
      clearInterval(loop);
-      
+    } 
     },10);
-
+}
     document.addEventListener("keydown", jump);
 
     const restartGame = () => {
